b14f0820ed5e1c77849cb7f8fae7a000be54be0c
shiyunjie <shiyunjie_nw@163.com>
2017-01-15 11:39:43 +0800

    2017-1-15 3 c

---
+++ ios/HttpRsaGzip.h
@@ -0,0 +1,14 @@
+//
+//  HttpRsaGzip.h
+//  HttpRsaGzip
+//
+//  Created by 施赟杰 on 17/1/15.
+//  Copyright © 2017年 com.runma. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "RCTBridgeModule.h"
+
+@interface HttpRsaGzip : NSObject<RCTBridgeModule>
+
+@end
